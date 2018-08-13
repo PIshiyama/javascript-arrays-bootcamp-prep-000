3f1f9e4d010727ecfa2943a3c353d3c18593cd34
Paul Ishiyama <paul.h.ishiyama@gmail.com>
2018-08-13 03:00:55 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -8,6 +8,7 @@ function addElementToBeginningOfArray(array, element) {
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
   array = array.unshift(element);
+  var element = element;
   return array;
 }
 
